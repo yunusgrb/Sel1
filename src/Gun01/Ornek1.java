da25eb70e95d5257a05b92f4ae9425463d89193e
TechnoStudy <ismettechno@gmail.com>
2022-07-05 18:37:12 +0100

    2.Örnek

--- src/Gun01/Ornek1.java
+++ src/Gun01/Ornek1.java
@@ -3,6 +3,8 @@ package Gun01;
 public class Ornek1 {
     public static void main(String[] args) {
         System.out.println("merhaba dünya");
+
+        System.out.println("merhaba 2");
     }
 
 }
